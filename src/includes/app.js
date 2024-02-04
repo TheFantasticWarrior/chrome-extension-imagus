8f9bd9184a448e93f5c6d415c1588635df3175fd
TheFantasticWarrior <39485135+TheFantasticWarrior@users.noreply.github.com>
2024-02-04 12:50:18 +0800

    german translation #60, update version number

--- src/includes/app.js
+++ src/includes/app.js
@@ -11,7 +11,7 @@ var platform = {},
 
 app = {
     name: /*~APP_NAME~*/ "Imagus mod",
-    version: /*~APP_VERSION~*/ "0.10.15",
+    version: /*~APP_VERSION~*/ "0.10.16",
 };
 
 if (document instanceof window.HTMLDocument) {
